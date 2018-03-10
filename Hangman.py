d442947c6c70d295d0253439564269c3eab3922d
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-03-10 22:26:34 +0530

    first commit

---
+++ Hangman.py
@@ -0,0 +1,28 @@
+import random
+answerlist = ["world","look","hello","goodbye"]
+random.shuffle(answerlist)
+answer = list(answerlist[0])
+
+display = []
+display.extend(answer)
+
+for i in range(len(display)):
+    display[i] = "_"
+print(' '.join(display))
+print()
+
+count = 0
+while count < len(answer):
+    guess = input("Please guess a letter: ")
+    guess = guess.lower()
+    print(count)
+
+    for i in range(len(answer)):
+        if answer[i] == guess :
+            display[i] = guess
+            count = count + 1
+
+    print(' '.join(display))
+    print()
+
+print("Well done, you guessed the word")
